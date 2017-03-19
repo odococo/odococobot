a464eb4f38bbe043f296cdb2cfcb9f82f3c22f33
odococo <ivan_lamperti@outlook.it>
2017-03-19 14:24:21 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -34,8 +34,9 @@ if(strpos($text, "/start") === 0) {
 	$response = "Comando non valido!";
 }
 
-$parameters = array('chat_id' => $chatId, "text" => $response . print_r($update, true));
-$parameters["method"] = "sendMessage";
+$parameters = array('chat_id' => $chatId, 'text' => $response);
+$parameters['method'] = "sendMessage";
 echo json_encode($parameters);
+$parameters['text'] = "print_r($update, true)";
 echo json_encode($parameters);
 ?>
