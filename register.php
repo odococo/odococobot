cbd61fa3bba93acb539a3c2ab27dc54ddd410272
odococo <ivan_lamperti@outlook.it>
2017-03-19 11:45:03 +0100

    Add files via upload

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://odococobot.herokuapp.com/execute.php';
+$BOT_TOKEN = '262354959:AAGZbji0qOxQV-MwzzRqiWJYdPVzkqrbC4Y';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
