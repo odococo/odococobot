ebab8ee94329a09b7c62b7ccb2bacdb5e2c4988a
odococo <ivan_lamperti@outlook.it>
2017-03-20 21:56:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,4 @@ $botToken = "262354959:AAGZbji0qOxQV-MwzzRqiWJYdPVzkqrbC4Y";
   $website = "https://api.telegram.org/bot".$botToken;
   //$content = file_get_contents("php://input");
   $content = file_get_contents($website);
+  print_r($content);
