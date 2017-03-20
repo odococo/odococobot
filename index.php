8e68d5ced61c9ca1825bf4c09c1a6589765f6ba5
odococo <ivan_lamperti@outlook.it>
2017-03-20 22:00:28 +0100

    Create index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php echo "Silence is golden";
 $botToken = "262354959:AAGZbji0qOxQV-MwzzRqiWJYdPVzkqrbC4Y";
   $website = "https://api.telegram.org/bot".$botToken;
-  //$content = file_get_contents("php://input");
-  $content = file_get_contents($website);
+  $content = file_get_contents("php://input");
+  //$content = file_get_contents($website);
   print_r($content);
   echo "ok";
