4eca54202037b5833c7f70a30708678dcb7f06e3
odococo <ivan_lamperti@outlook.it>
2017-03-20 11:48:37 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,7 +1,6 @@
 <?php
 
 $botToken = "262354959:AAGZbji0qOxQV-MwzzRqiWJYdPVzkqrbC4Y";
-$website = "https://api.telegram.org/bot".$botToken;
 
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
@@ -50,15 +49,8 @@ echo json_encode($parameters);*/
 function sendMessage($chatID, $messaggio, $token) {
     echo "sending message to " . $chatID . "\n";
 
-    $url = "https://api.telegram.org/" . $token . "/sendMessage?chat_id=" . $chatID;
+    $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatID;
     $url = $url . "&text=" . urlencode($messaggio);
-    $ch = curl_init();
-    $optArray = array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true
-    );
-    curl_setopt_array($ch, $optArray);
-    $result = curl_exec($ch);
-    curl_close($ch);
+    file_get_contents($url);
 }
 ?>
