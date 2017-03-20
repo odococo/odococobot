496522e08d192f8ec0c3303a21deb362b15b6d52
odococo <ivan_lamperti@outlook.it>
2017-03-20 11:44:16 +0100

    Create execute.php

--- execute.php
+++ execute.php
@@ -23,7 +23,7 @@ $text = isset($message['text']) ? $message['text'] : "";
 $text = trim($text);
 $text = strtolower($text);
 
-//header("Content-Type: application/json");
+header("Content-Type: application/json");
 
 if(strpos($text, "/start") === 0) {
 	$response = "Ciao $firstname, benvenuto!";
