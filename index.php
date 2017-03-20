844130fce34e70a2e3e9074f9e993c18d21a1e08
odococo <ivan_lamperti@outlook.it>
2017-03-20 21:53:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
 <?php echo "Silence is golden";
+$botToken = "262354959:AAGZbji0qOxQV-MwzzRqiWJYdPVzkqrbC4Y";
+  $website = "https://api.telegram.org/bot".$botToken;
+  //$content = file_get_contents("php://input");
+  $content = file_get_contents($website);
