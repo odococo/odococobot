1990affd041842a0a780031aac55f1e4d2550ddb
odococo <ivan_lamperti@outlook.it>
2017-03-21 11:13:57 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -42,21 +42,6 @@
       } else {
         apiRequest("sendMessage", array('chat_id' => $chat_id, 'text' => "Comando non valido"));
       }
-
-      if (strpos($text, "/start") === 0) {
-        apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Hello', 'reply_markup' => array(
-          'keyboard' => array(array('Hello', 'Hi')),
-          'one_time_keyboard' => true,
-          'resize_keyboard' => true)));
-      } else if ($text === "Hello" || $text === "Hi") {
-        apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Nice to meet you'));
-      } else if (strpos($text, "/stop") === 0) {
-        // stop now
-      } else {
-        apiRequestWebhook("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => 'Cool'));
-      }
-    } else {
-      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'I understand only text messages'));
     }
   }
   
